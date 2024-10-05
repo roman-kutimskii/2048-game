b96d900564a232b9be2b267cab9dfa10b12d4a96
Roman Kutimskii <roman.kutimskii@gmail.com>
2024-10-05 15:13:06 +0300

    Add comments; add .dockerignore

--- run.bat
+++ run.bat
@@ -2,3 +2,7 @@ docker pull node:14
 docker build -t 2048game:latest .
 docker run --rm -d -p 8080:8080 2048game:latest
 docker run --rm -d -p 8081:8080 2048game:latest
+
+@REM --rm - удаляет после остановки
+@REM -d - запускает в фоне
+@REM -p делает порты
